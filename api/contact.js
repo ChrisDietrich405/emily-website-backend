ee5f42f0fb45c57b7e1fd6de6926e5761346c872
Chris <chrisdietrich3666@gmail.com>
2022-12-13 05:26:59 -0500

    test

--- api/contact.js
+++ api/contact.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 var transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: 'smtp.sendgrid.net',
+  port: 587,
   auth: {
-    user: "fb4e361370b4e6",
-    pass: "be414bfafbc4e5",
+    user: "apikey",
+    pass: "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",
   },
 });
 
@@ -22,14 +22,22 @@ var transport = nodemailer.createTransport({
 class ContactApi {
   sendForm(req, res) {
     let content = fs.readFileSync(__dirname + "/contact-form-template.html");
-    console.log(new Buffer.from(content).toString());
-    var mailOptions = {
+    // console.log(new Buffer.from(content).toString());
+    let mailOptions = {
       from: req.body.email,
       to: "chrisdietrich366@yahoo.com",
       subject: `Contact form from ${req.body.firstName} ${req.body.lastName}`,
       text: req.body.message,
-      html: content,
+      html: "hello",
     };
+    // console.log(mailOptions)
+    // transport.verify(function (error, success) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log("Server is ready to take our messages");
+    //   }
+    // });
     transport.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log(error);
